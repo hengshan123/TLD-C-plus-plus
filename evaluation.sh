b8c3d6e5e25d360af6f93736d79d4e387b0a5dcd
jhs <879302317@qq.com>
2014-08-25 17:25:42 +0800

    first commit

---
+++ evaluation.sh
@@ -0,0 +1,2 @@
+cd datasets/
+python  evaluate_vis.py  11_pets/PETS-550-749.avi 11_pets/output-550-749.txt 11_pets/gt_550_749.txt 11_pets/result.txt
---
+++ run.sh
@@ -0,0 +1,3 @@
+cd bin/
+./run_tld  -p ../parameters.yml -s ../datasets/11_pets/PETS-550-749.avi -b ../datasets/11_pets/init.txt -tl
+#./run_tld -p ../parameters.yml -s ../datasets/01_david/david.mpg -b ../datasets/01_david/init.txt -tl
